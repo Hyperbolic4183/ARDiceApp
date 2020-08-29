658b144d32096afe2715e0692f421afa2a73a8a8
otsuka.amane <otsuka.amane@gmail.com>
2020-08-29 09:38:42 +0900

    Initial Commit

---
+++ ARtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARtest
+//
+//  Created by 大塚周 on 2020/08/29.
+//  Copyright © 2020 大塚周. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
